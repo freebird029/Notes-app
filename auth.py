a83e9ffa36b806872d3bc92f27e9e1ae1b3c1c98
freebird029 <darnelle.sithi@gmail.com>
2024-11-01 16:20:24 +0200

    updated the code

--- auth.py
+++ auth.py
@@ -2,6 +2,18 @@ from flask import Blueprint
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/")
-def home():
-    return "<h1>Test</h1>"
+# @auth.route("/")
+# def home():
+#     return "<h1>Test</h1>"
+
+@auth.route("/login")
+def login():
+    return "<p>login</p>"
+
+@auth.route("/logout")
+def logout():
+    return "<p>logout</p>"
+
+@auth.route("/sign_up")
+def sign_up():
+    return "<p>Sign up</p>"
